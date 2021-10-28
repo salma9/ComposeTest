7d2e93d04723d74e3eaaa6cf071174e9b0849b69
makni salma <makni.salma9@gmail.com>
2021-10-28 17:19:42 +0200

    hello Android

---
+++ app/src/main/java/com/example/testcompose/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.testcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.testcompose.ui.theme.TestComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TestComposeTheme {
+                myApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun myApp(){
+    // A surface container using the 'background' color from the theme
+    Surface(color = MaterialTheme.colors.background) {
+        Greeting("Android")
+    }
+}
+
+@Composable
+private fun Greeting(name: String) {
+    Surface(color = MaterialTheme.colors.primary) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
+}
+
+@Preview(showBackground = true, name = "Main preview")
+@Composable
+fun DefaultPreview() {
+    myApp()
+}
